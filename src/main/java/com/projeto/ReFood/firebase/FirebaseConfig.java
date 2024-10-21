3bb45118f51845d41763a14ceb64a26a2e639d80
Marina Barbosa <131506431+marina-barbosa@users.noreply.github.com>
2024-10-21 18:49:50 -0300

    Update FirebaseConfig.java

--- src/main/java/com/projeto/ReFood/firebase/FirebaseConfig.java
+++ src/main/java/com/projeto/ReFood/firebase/FirebaseConfig.java
@@ -1,27 +1,27 @@
-package com.projeto.ReFood.firebase;
+// package com.projeto.ReFood.firebase;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+// import java.io.FileInputStream;
+// import java.io.IOException;
 
-@Configuration
-public class FirebaseConfig {
+// @Configuration
+// public class FirebaseConfig {
 
-  @Bean
-  public FirebaseApp initializeFirebase() throws IOException {
-    String serviceAccountPath = System.getProperty("user.dir") + "/refoods/refood-firebase-key.json";
-    FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath);
+//   @Bean
+//   public FirebaseApp initializeFirebase() throws IOException {
+//     String serviceAccountPath = System.getProperty("user.dir") + "/refoods/refood-firebase-key.json";
+//     FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath);
 
-    FirebaseOptions options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
-        .setStorageBucket("refood-refood.appspot.com")
-        .build();
-    return FirebaseApp.initializeApp(options);
-  }
+//     FirebaseOptions options = FirebaseOptions.builder()
+//         .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+//         .setStorageBucket("refood-refood.appspot.com")
+//         .build();
+//     return FirebaseApp.initializeApp(options);
+//   }
 
-}
+// }
