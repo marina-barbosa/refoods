eecf8007b3e4c6192e16ff6005080e9617885764
Marina Barbosa <131506431+marina-barbosa@users.noreply.github.com>
2024-10-21 18:50:01 -0300

    Update FirebaseService.java

--- src/main/java/com/projeto/ReFood/firebase/FirebaseService.java
+++ src/main/java/com/projeto/ReFood/firebase/FirebaseService.java
@@ -1,32 +1,32 @@
-package com.projeto.ReFood.firebase;
+// package com.projeto.ReFood.firebase;
 
-import com.google.cloud.storage.Bucket;
-import com.google.firebase.cloud.StorageClient;
+// import com.google.cloud.storage.Bucket;
+// import com.google.firebase.cloud.StorageClient;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
+// import java.io.IOException;
+// import java.io.InputStream;
 
-@Service
-public class FirebaseService {
+// @Service
+// public class FirebaseService {
 
-  @Value("${image.base.url}")
-  private String imageBaseUrl;
+//   @Value("${image.base.url}")
+//   private String imageBaseUrl;
 
-  public void upload(MultipartFile imageFile, String imageName) throws IOException {
-    InputStream inputStream = imageFile.getInputStream();
-    Bucket bucket = StorageClient.getInstance().bucket();
-    bucket.create(imageName, inputStream, "image/jpeg");
-  }
+//   public void upload(MultipartFile imageFile, String imageName) throws IOException {
+//     InputStream inputStream = imageFile.getInputStream();
+//     Bucket bucket = StorageClient.getInstance().bucket();
+//     bucket.create(imageName, inputStream, "image/jpeg");
+//   }
 
-  public String getImageUrl(String imageName, String firebaseToken) {
-    return imageBaseUrl + imageName + "?alt=media&token=" + firebaseToken;
-  }
+//   public String getImageUrl(String imageName, String firebaseToken) {
+//     return imageBaseUrl + imageName + "?alt=media&token=" + firebaseToken;
+//   }
 
-}
+// }
 
 
 
